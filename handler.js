c43236969ccdef2b37e5f66d2cab8ce058a07c5c
Fernando Medina Corey <fmcorey@gmail.com>
2017-06-29 17:14:57 -0700

    Add event, new handler contents, static site.

--- handler.js
+++ handler.js
@@ -1,6 +1,14 @@
 'use strict';
 
-module.exports.process = (event, context, callback) => {
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
+
+
+module.exports.validateRecaptchaSendSms = (event, context, callback) => {
+
+
     // Process event and get values out
     var google_token = event["google_token"]
     var sms_message = event["sms_message"]
@@ -10,33 +18,46 @@ module.exports.process = (event, context, callback) => {
 
     // Get fail if Google recaptcha token fails
     // If err with the google token not being vaild
-    const failure_response = {
-        statusCode: 500,
+
+  // use twilio SDK to send text message
+  const sms = {
+    to: event.body.to,
+    body: event.body.message || '',
+    from: twilioPhoneNumber,
+  };
+  // add image to sms if supplied
+  if (event.body.image) {
+    sms.mediaUrl = event.body.image;
+  }
+  twilioClient.messages.create(sms, (error, data) => { // eslint-disable-line
+    if (error) {
+      const errResponse = {
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        },
+        statusCode: error.status,
         body: JSON.stringify({
-            message: 'The recaptcha failed. Please try again!',
-            input: event,
+          message: error.message,
+          error: error // eslint-disable-line
         }),
+      };
+      return callback(null, errResponse);
+    }
+    // text message sent! ✅
+    console.log(`message: ${data.body}`); // eslint-disable-line
+    console.log(`date_created: ${data.date_created}`); // eslint-disable-line
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      },
+      body: JSON.stringify({
+        message: 'Text message successfully sent!',
+        data: data // eslint-disable-line
+      }),
     };
-
-    // Setup Twilio
-    // something like var twilio = require('twilio')
-
-
-    // Send twillio message (if google recaptcha passes)
-
-
-    // On success
-    const success_response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Your message was sent successfully!',
-            input: event,
-        }),
-    };
-
-    
 
     callback(null, response);
-
-    
+  });
 };
