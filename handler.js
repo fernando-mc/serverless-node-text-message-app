6e16834cbb49af9d9a64a52f6b944b4ee900cef4
Fernando Medina Corey <fmcorey@gmail.com>
2017-07-07 13:03:26 -0700

    Update to include recaptcha validation

--- handler.js
+++ handler.js
@@ -1,80 +1,93 @@
-'use strict';
+'use strict'; 
 
+// Load environment variables
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+const recaptchaSecret = process.env.GOOGLE_RECAPTCHA_TOKEN;
+
+// Setup dependencies
 const request = require('request');
 const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken);
-const recaptchaSecret = process.env.GOOGLE_RECAPTHCA_SECERT;
-const recaptchaUrl = 'https://www.google.com/recaptcha/api/siteverify';
 
-module.exports.validateRecaptchaSendSms = (event, context, callback) => {    
-    // node request lib
-    const recaptchaData = {
-            'secret': GA_SECRET, 
-            'response': event['captcha']
-          }
-    request({
-      url: recaptchaUrl,
-      method: 'POST',
-      json: true,
-      body: recaptchaData,
-    }, function (error, response, body){
-      console.log(response);
-      console.log(body);
-      const recaptchaStatus = response['success']
-    });
+// Required in responses for CORS support to work
+const headers = {'Access-Control-Allow-Origin': '*'};
 
-
-    // Process event and get values out
-    var google_token = event["google_token"]
-    var sms_message = event["sms_message"]
-    // confirm that the values pass into this from the form
-
-    // process google_token
-
-    // Get fail if Google recaptcha token fails
-    // If err with the google token not being vaild
-
-  // use twilio SDK to send text message
-  const sms = {
-    to: event.body.to,
-    body: event.body.message || '',
-    from: twilioPhoneNumber,
+module.exports.validateAndSend = (event, context, callback) => {
+  
+  const validationData = {
+    url: 'https://www.google.com/recaptcha/api/siteverify?secret=' + 
+      recaptchaSecret + "&response=" + event.body.captcha,
+    method: 'POST',
   };
-  // add image to sms if supplied
-  if (event.body.image) {
-    sms.mediaUrl = event.body.image;
-  }
-  twilioClient.messages.create(sms, (error, data) => { // eslint-disable-line
-    if (error) {
-      const errResponse = {
-        headers: {
-          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        },
-        statusCode: error.status,
+
+  request(validationData, function(error, response, body) {
+    const parsedBody = JSON.parse(body)
+
+    if (error || response.statusCode !== 200){
+
+      const recaptchaErrResponse = {
+        headers: headers,
+        statusCode: 500,
         body: JSON.stringify({
-          message: error.message,
-          error: error // eslint-disable-line
+          status: 'fail',
+          message: 'Error attempting to validate recaptcha.',
+          error: error || response.statusCode
         }),
       };
-      return callback(null, errResponse);
+
+      return callback(null, recaptchaErrResponse);
+    } else if (parsedBody.success === false) {
+      
+      const recaptchaFailedErrResponse = {
+        headers: headers,
+        statusCode: 200,
+        body: JSON.stringify({
+          status: 'fail',
+          message: 'Captcha validation failed. Refresh the page & try again!',
+        })
+      };
+      
+      return callback(null, recaptchaFailedErrResponse);
+    } else if (parsedBody.success === true) {
+      
+      // Try to actually send the message
+      const sms = {
+        to: event.body.to,
+        body: event.body.message || '',
+        from: twilioPhoneNumber,
+      };
+
+      twilioClient.messages.create(sms, (error, data) => {
+        if (error) {
+
+          const twilioErrResponse = {
+            headers: headers,
+            statusCode: 200,
+            body: JSON.stringify({
+              status: 'fail',
+              message: error.message,
+              error: error
+            })
+          };
+
+          return callback(null, twilioErrResponse);
+        }
+        // If no errors: Return success response!
+        
+        const successResponse = {
+          headers: headers,
+          statusCode: 200,
+          body: JSON.stringify({
+            status: 'success',
+            message: 'Text message successfully sent!',
+            body: data.body,
+            created: data.dateCreated
+          })
+        };
+
+        callback(null, successResponse);
+      });
     }
-    // text message sent! ✅
-    console.log(`message: ${data.body}`); // eslint-disable-line
-    console.log(`date_created: ${data.date_created}`); // eslint-disable-line
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      },
-      body: JSON.stringify({
-        message: 'Text message successfully sent!',
-        data: data // eslint-disable-line
-      }),
-    };
-
-    callback(null, response);
-  });
+  })
 };
